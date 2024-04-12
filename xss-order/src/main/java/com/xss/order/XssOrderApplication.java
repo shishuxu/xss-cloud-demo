3473cc7558e2795dbe8ea6be0d17faac7c008a5d
xu457 <457683048@qq.com>
2024-04-12 16:52:14 +0800

    石书

---
+++ xss-order/src/main/java/com/xss/order/XssOrderApplication.java
@@ -0,0 +1,13 @@
+package com.xss.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XssOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XssOrderApplication.class, args);
+    }
+
+}
