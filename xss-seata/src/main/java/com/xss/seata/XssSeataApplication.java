e9de3879c2c2510ad026cb6054d292ac42a96be6
xu457 <457683048@qq.com>
2024-04-15 18:45:19 +0800

    //

---
+++ xss-seata/src/main/java/com/xss/seata/XssSeataApplication.java
@@ -0,0 +1,13 @@
+package com.xss.seata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XssSeataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XssSeataApplication.class, args);
+    }
+
+}
---
+++ xss-seata/src/test/java/com/xss/seata/XssSeataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xss.seata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XssSeataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
